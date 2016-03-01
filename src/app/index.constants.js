4801ca5a497388654dbfbb9076dcea20a09ad800
OlegUA96 <zhuros96@gmail.com>
2016-03-01 20:55:32 +0200

    done adding and removing at angulare

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,10 +1,1 @@
 /* global malarkey:false, moment:false */
-(function() {
-  'use strict';
-
-  angular
-    .module('levelUpAngular')
-    .constant('malarkey', malarkey)
-    .constant('moment', moment);
-
-})();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,8 +9,46 @@
   function MainController() {
     var vm = this;
 
-    vm.header = "ToDo List";
-    vm.newTodo = null;
+    vm.header = "TOTOLIST";
+    vm.btnText = "ADD";
+    vm.items = [
+      {
+        name: "First",
+        completed: false,
+        id: 0
+      },
+      {
+        name: "Second",
+        completed: false,
+        id: 1
+      },
+      ]
+    vm.newTodo = "Test todo";
     
+    vm.addItem = function (e) {
+      var model = null;
+
+      if(e.keyCode === 13 || e.type === "click") {
+        model = {
+          name: vm.newTodo,
+          completed: false,
+          id: generateId()
+        };
+      vm.items.push(model);
+      vm.newTodo = null;
+      }
+    };
+
+    vm.deleteItem = function (e, id) {
+      var currentIndex = this.items.indexOf(this.items.filter(function (item) {
+            return item.id === parseInt(id);
+        })[0]);
+
+      vm.items.splice(currentIndex, 1);
+    }
+    function generateId () {
+        return Math.floor((1 + Math.random()) * 0x10000);
+    }
+
   }
 })();
